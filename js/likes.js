c4569897fa284920aee1277f9ea0161e8294a8a1
Xavier Biscay <xbdevweb@gmail.com>
2023-05-26 13:52:27 +0200

    xav

--- js/likes.js
+++ js/likes.js
@@ -1,11 +1,7 @@
-fetch("https://music.freefakeapi.io/api/tracks?page=1&nopaginate=false&order=latest&limit=10", {
-  headers: { 
-    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo4LCJlbWFpbCI6Im0udGFocmk1NTJAZ21haWwuY29tIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY4NDk5NjM3MSwiZXhwIjoxNjg1MDMyMzcxfQ.m8lCMp4va3nuAtKfRal9VHP3NGlsOVdo3oWmIkPHTlc"
+fetch("https://music.freefakeapi.io/api/tracks?page=1&nopaginate=false&order=latest&limit=100", {
+  headers: {
+    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo4LCJlbWFpbCI6Im0udGFocmk1NTJAZ21haWwuY29tIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY4NTA4MTM1OSwiZXhwIjoxNjg1MTE3MzU5fQ.BmvmzMk7O78l2mcHZQHVPiPci2g3b9RoSRuUGzbszl0"
   }
 })
-.then(reponse => reponse.json())
-.then(truc => {
-  for (let id = 0; id < 10; id++){
-    document.querySelector(".favoris").insertAdjacentHTML("beforeend", "<img src='https://music.freefakeapi.io" + truc[id].cover + "'>");
-  }
-})
+  .then(reponse => {
+    console.log(reponse)})
