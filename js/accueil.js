aefbaf07a839832f83dfb6d803120547b9678e55
Xavier Biscay <xbdevweb@gmail.com>
2023-06-02 14:26:20 +0200

    xav

--- js/accueil.js
+++ js/accueil.js
@@ -1784,3 +1784,45 @@ coeurplein.addEventListener("click", function () {
     }
   }).then(reponse => reponse.json())
 })
+let random = document.querySelector(".random_music");
+
+random.addEventListener("click", function () {
+  var numeroAleatoire = Math.floor(Math.random() * 60) + 1;
+
+  fetch("https://music.freefakeapi.io/api/tracks?order=alpha&limit=100", {
+    headers: {
+      Authorization: "Bearer " + sessionStorage.token
+    }
+  })
+    .then(reponse => reponse.json()).then((tracks) => {
+      var numeroAleatoire = Math.floor(Math.random() * Object.keys(tracks).length);
+
+      let idMusic = tracks[numeroAleatoire].id;
+      let player = document.querySelector("audio")
+      if (!player.paused) {
+        player.pause();
+        document.querySelector(".pause_lecture").style.display = "block";
+        document.querySelector(".lecture_pause").style.display = "none";
+      }
+
+      fetch("https://music.freefakeapi.io/api/tracks/" + idMusic, {
+        headers: {
+          Authorization: "Bearer " + sessionStorage.token
+        }
+      })
+        .then(reponse => reponse.json())
+        .then(track => {
+          console.log(track)
+          document.querySelector(".cover").src = "https://music.freefakeapi.io" + track.cover;
+          document.querySelector(".title").textContent = track.title;
+          player.src = "https://music.freefakeapi.io" + track.file
+
+        })
+    })
+
+
+
+
+
+
+})
--- js/likes.js
+++ js/likes.js
@@ -1,17 +1,43 @@
 let random = document.querySelector(".random_music");
 
 random.addEventListener("click", function () {
-var numeroAleatoire = Math.floor(Math.random() * 60) + 1;
-// console.log(numeroAleatoire)
-fetch("https://music.freefakeapi.io/api/tracks/" + numeroAleatoire, {
-          headers: {
-            Authorization: "Bearer " + sessionStorage.token
-          }
-        })
-          .then(reponse => reponse.json())
-          .then(reponse => reponse.json())
-          console.log(reponse)
+  var numeroAleatoire = Math.floor(Math.random() * 60) + 1;
 
-  
+  fetch("https://music.freefakeapi.io/api/tracks?order=alpha&limit=100", {
+    headers: {
+      Authorization: "Bearer " + sessionStorage.token
+    }
+  })
+    .then(reponse => reponse.json()).then((tracks) => {
+      var numeroAleatoire = Math.floor(Math.random() * Object.keys(tracks).length);
+
+      let idMusic = tracks[numeroAleatoire].id;
+      let player = document.querySelector("audio")
+      if (!player.paused) {
+        player.pause();
+        document.querySelector(".pause_lecture").style.display = "block";
+        document.querySelector(".lecture_pause").style.display = "none";
+      }
+
+      fetch("https://music.freefakeapi.io/api/tracks/" + idMusic, {
+        headers: {
+          Authorization: "Bearer " + sessionStorage.token
+        }
+      })
+        .then(reponse => reponse.json())
+        .then(track => {
+          console.log(track)
+          document.querySelector(".cover").src = "https://music.freefakeapi.io" + track.cover;
+          document.querySelector(".title").textContent = track.title;
+          player.src = "https://music.freefakeapi.io" + track.file
+
+        })
     })
- 
+
+
+
+
+
+
+})
+
